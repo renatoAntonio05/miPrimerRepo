3a33eb30ac7b76a174cc55752f616c56dd61c935
Renato Antonio <tony.z230591@gamil.com>
2025-03-18 19:10:23 -0600

    Se agrego bucle para verificar que no tenga espacios en blanco

--- renato/login.py
+++ renato/login.py
@@ -11,17 +11,31 @@ password_user_db= "12345678"
 # 3. Validar que el email y contraseña sean correctos
 # 4. Si son correctos imprimir "Bienvenido"
 # 5. Si no son correctos imprimir "Email o contraseña incorrectos"
+# 6. No se admiten espacios vasios
 
-name_user = input('ingresa tu usuario-> ')
-password = ""
 
+# Este bucle tiene una condicional para cerciorarse que los campos no vayan en blanco.
+while True:
+    name_user = input('ingresa tu usuario-> ').strip().lower()
+    if name_user:
+        break
+    print('no se admiten espacios vacios')
+
+# Esta bucle tiene una condicional para cerciorarse que los campos no vayan en blanco.
+while True:
+    password = input('ingresa tu password-> ').strip()
+    if password:
+        break
+    print('no se admiten espacios vacios')
 
 if name_user.strip().lower() == email_user_db.strip().lower():
-    password = input('Ingresa tu contraseña -> ')
     if password == password_user_db:
         print('Bienvenido')
     else:
         print('Email o contraseña es incorrectos')
 else:
-    print('El usuario no existe')
+    print('No ingresaste Email o contraseña correcto')
+
+
+
 
