b3a04a9fefb9d57758cbbfa13954505487029b37
Daniel Antonio <daniel23.da74@gmail.com>
2025-03-19 22:03:13 -0600

    feat(validation): improve empty value validation messages

--- renato/helpers/validation_not_empty.py
+++ renato/helpers/validation_not_empty.py
@@ -1,3 +1,8 @@
 def validation_not_empty(value, message):
     if value == '':
         print(message)
+
+
+def validation_not_empty_default_message(value):
+    if value == '':
+        print('El valor no puede venir vacio')
--- renato/login-function.py
+++ renato/login-function.py
@@ -19,10 +19,10 @@ name_user = input('ingresa tu usuario-> ').strip().lower()
 password = input('ingresa tu password-> ')
 
 # Validar que el email no venga vacio.
-validation.validation_not_empty(name_user,'Falta usuario')
+validation.validation_not_empty(name_user,'el valor debe tener algo')
 
 # Validar que la contraseña no venga vacia.
-validation.validation_not_empty(password,'Falta enviar una contraseña')
+validation.validation_not_empty_default_message(password)
 
 validation_email_password(email_user_db,password_user_db,name_user,password)
 
