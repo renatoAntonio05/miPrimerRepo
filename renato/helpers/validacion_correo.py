4c914203882b06fbdf765feea0750ea6f4a74bed
Renato Antonio <tony.z230591@gamil.com>
2025-03-23 17:40:41 -0600

    Se agrego un modulo

--- renato/helpers/validacion_correo.py
+++ renato/helpers/validacion_correo.py
@@ -1,7 +1,11 @@
 import re
 
-def validar_correo(messenger):
+def validar_correo(email):
     patron = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
-    if re.match(patron, messenger):
-        return bool(re.match(patron, messenger))
+    if re.match(patron, email) is None:
+        print('Esto no es un correo')
+        exit(0)
 
+
+
+# return para devolver la informacion
--- renato/login-function.py
+++ renato/login-function.py
@@ -7,7 +7,13 @@ from helpers import validacion_correo as correo
 values = db.get_info_db()
 
 name_user = inputs.get_information_email_input('Ingresa tu email:')
+
+
 password = inputs.get_information_input('Ingresa tu password:')
+
+# se llama a la funcion validacion de corroe
+correo.validar_correo(name_user)
+
 
 # Validar que el email no venga vacio.
 validation.validation_not_empty(name_user,'el valor debe tener algo')
@@ -17,4 +23,3 @@ validation.validation_not_empty_default_message(password)
 
 verificacion.validation_email_password(values['email'],values['password_user_db'],name_user,password)
 
-correo.validar_correo(values['email'])
