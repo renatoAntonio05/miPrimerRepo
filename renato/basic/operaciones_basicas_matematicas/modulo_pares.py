3a45a19ee2b36f17fe613ae8dda5b0333bdb2506
Daniel Antonio <daniel23.da74@gmail.com>
2025-03-25 13:17:49 -0600

    feat(operaciones-basicas): agregando comentarios

--- renato/basic/operaciones_basicas_matematicas/modulo_pares.py
+++ renato/basic/operaciones_basicas_matematicas/modulo_pares.py
@@ -1,12 +1,11 @@
-
-
+# TODO: Este no quiero saber si es par solo quiero obtener el residuo.
 def modulo(num_1, num_2):
-    if num_1 %2 == 0:
+    if num_1 % 2 == 0:
         print("El numero es par")
     else:
         print("El numero es inpar")
 
-    if num_2 % 2 ==0:
+    if num_2 % 2 == 0:
         print("El numero es par")
     else:
         print("El numero es impar")
--- renato/basic/operaciones_basicas_matematicas/operaciones_basicas.py
+++ renato/basic/operaciones_basicas_matematicas/operaciones_basicas.py
@@ -7,15 +7,22 @@ import modulo_multiplicaicon as modulo_4
 num_1 = 2
 num_2 = 7
 
+print('Suma: ')
+mas.suma(num_1, num_2)
+print("_____________________________________")
 
-mas.suma(num_1,num_2)
+print('Resta:')
+modulo_2.resta(num_1, num_2)
 print("_____________________________________")
-modulo_2.resta(num_1,num_2)
-print("_____________________________________")
+
+print('Division: ')
 division_1.division(num_1, num_2)
 print("_____________________________________")
+
+print('Modulo (residuo): ')
 modulo_3.modulo(num_1, num_2)
 print("_____________________________________")
+
+print('Multiplicación: ')
 modulo_4.multiplicacion(num_1, num_2)
 print("_____________________________________")
-
