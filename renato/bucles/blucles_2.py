a0ad1a01758adeba5aecae62e97ffc176e36975e
Daniel Antonio <daniel23.da74@gmail.com>
2025-04-05 21:55:42 -0600

    feat(blucles_2): mejorar impresión de frutas y contar caracteres

--- renato/bucles/blucles_2.py
+++ renato/bucles/blucles_2.py
@@ -1,24 +1,30 @@
 # es el conjunto de frutas
-from IPython.lib.pretty import Printable
 
 frutas = ["manzana", "naranja", "piña", "sandia", "pera", "melon", "platano", "papaya", "naranja", "guayaba", "mango"]
 
 # inicializamos el tamaño de la varible
-size = 0
+index_of_frutas = 0
+
 # con el for recorremos la lista
 for fruta in frutas:
-    #incrementa la variable en cada bucle
-    size = size + 1  # TODO: esto lo haces para saber cual es el index de las frutas
+    # incrementa la variable en cada bucle
+    index_of_frutas = index_of_frutas + 1
+
     # se imprime el elemento de la lista y el tamaño
-    print(size, fruta)
+    print('indice de la fruta es: ', index_of_frutas, 'nombre fruta:', fruta)
+
 print('-------------------------------------------------------------')
 ########################################################################################################
+
 # la lista se opcupa un varible
 len_mylist = len(frutas)
+
 # el otro metodo es mediante un ciclo
 for x in range(len_mylist):
-    print(frutas[x])
+    print('Nombre de la fruta:', frutas[x])
+
 print("--------------------------------------------------------------")
+
 ########################################################################################################
 # este codigo se cuentan los elementos de cada uno de los strings
 # total_caracteres sera la variable con la que se guardan lo recorrido por el ciclo
@@ -26,11 +32,8 @@ total_caracteres = 0
 # el ciclo que recorre la lista
 for fruto in frutas:
     # con esta linea de codigo se cuenta los caractres de cada uno de los string
-    # TODO: Esta bien solo que quiero que imprimas las que tiene 7 o mas caracteres.
-    # TODO: Tenemos que hacer una validación
     total_caracteres = len(fruto)
     # con esta condicion se imprime unicamnete las palabras que son mayores a
-    if total_caracteres >= 7:  #
-        print(fruto)
+    if total_caracteres >= 7:
         # se imprime el resultado
-
+        print('Fruta que tiene mas de 7 caracteres:', fruto)
