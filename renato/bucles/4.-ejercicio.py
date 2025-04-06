d79712af0ae4e312d0a4cec5271bbdcf0a1473f4
Daniel Antonio <daniel23.da74@gmail.com>
2025-04-05 22:38:54 -0600

    feat(ejercicio): add random number guessing game with hints

---
+++ renato/bucles/4.-ejercicio.py
@@ -0,0 +1,14 @@
+"""
+Genera un número aleatorio del 1 al 100 y permite al usuario adivinar. Da pistas (“más alto” o “más bajo”) hasta que lo adivine.
+"""
+
+"""
+        ?##### PASOS PARA RESOLVER EL PROBLEMA. #####
+        
+    Generar un número rando que este entre 1 y 100 (int)
+    Recibir un numero del usuario. (str -> int) (str != int)
+    Validar que sea el mismo numero.
+        Si es el mismo numero: ganaste. (Condicional)
+        Si no es el mismo numero y es mas grande: el numero es mas pequeño. (Condicional)
+        Si no es el mismo numero y es mas pequeño: el numero es mas grande. (Condicional)
+"""
