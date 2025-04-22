6e9bd81901959dc98a45fe75c4d2b6e49585c194
Daniel Antonio <daniel23.da74@gmail.com>
2025-04-21 22:52:19 -0600

    feat(ejercicios): subiendo ejercicios

---
+++ ejercicios.py
@@ -0,0 +1,34 @@
+"""
+1.- Crear una función que devuelva el cuadrado de un número.
+Pista: Investigar como hacer al cuadrado un número.
+"""
+
+"""
+2.- Verificar si un número es positivo, negativo o cero
+"""
+
+"""
+3.- Crear una lista de números pares del 1 al 20 usando un bucle
+Pista: 
+    - Recuerda como saber que es un número par.
+    - Recuerda que debes agregarlo en una lista.
+"""
+
+"""
+4.- Contar el número de vocales en una cadena de texto
+Pista: Debemos tener un contador que guarde el numero de vocales. 
+"""
+
+"""
+5.- Crear una función que verifique si una lista está vacía
+Pista: Recuerda como debemos saber la logitud de una lista.
+"""
+
+"""
+6.- Encontrar el número máximo en una lista sin usar funciones incorporadas
+"""
+
+"""
+ 7.- Encontrar la longitud de la palabra más larga en una lista
+ Pista:  existe una funcion llamada max() -> investiga para poder resolverlo.
+"""
