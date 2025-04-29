62d9a1d4a225a4fccbf0ca78d0ec768bbf3407b1
Renato Antonio <tony.z230591@gamil.com>
2025-04-28 23:28:29 -0600

    se agrego ejercicio 2

--- renato/Ejercicios_2/lista_mas_larga.py
+++ renato/Ejercicios_2/lista_mas_larga.py
@@ -3,13 +3,20 @@
 Pista: Existe una función llamada max() -> investiga para poder resolverlo.
 """
 
-lista_palabras = ["hola", "renato", "crepúsculo", " te quiero", "terrenal"]
+lista_palabras = ["hola", "renato", "crepúsculo", " te quiero", "terrenal", "ininterrumpidamente"]
 
-palabra = max(lista_palabras, key=len)
-print("la palabra mas larga es =>", {palabra})
+#palabra = max(lista_palabras, key=len)
+#print("la palabra mas larga es =>", {palabra})
 
 # TODO: Ahora hazlo con ciclos.
-palabra_nombre = ''
+
+
+palabra_mas_larga = " "
+for palabra in lista_palabras:
+    if len(palabra) > len(lista_palabras):
+        palabra_mas_larga = palabra
+print(palabra_mas_larga)
+
 
 # cliclo que recorra toda la lista
 # Saber que tan grande es la palabra que esta recorriendo
